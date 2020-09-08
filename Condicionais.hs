d0701df8b907430d679df6e565aa2fac2a78a30d
Leandro Teodoro <leandroteodoro.rj@gmail.com>
2020-09-07 23:06:00 -0300

    Update Condicionais.hs

--- Condicionais.hs
+++ Condicionais.hs
@@ -4,7 +4,6 @@
 -- é obrigatória. Isso implica dizer que o IF sempre retornará
 -- alguma coisa.
 
-
 -- Exemplo:
 dobraMenorQueCem x = if x > 100  
                    then x  
@@ -15,3 +14,31 @@ dobraMenorQueCem x = if x > 100
 
 -- Também é possível escrever em 1 só linha
 diminuiUmMenorQueCem x = if x > 100 then x-1 else x
+
+
+-- ESTRUTURA CONDICIONAL UTILIZANDO GUARDA
+
+guarda var
+  | var < 10 = "Seu numero pertence as unidades"
+  | var >= 10 && var < 100 = "Seu numero pertence as centenas"
+  | otherwise = "Seu numero e maior que cem"
+
+-- OBS: A linha otherwise é obrigatória e será executada se 
+-- nenhuma linha for atendida.
+
+-- É possível utilizar funções dentro da guarda
+media trab prova 
+  | med < 6 = "Voce foi reprovado"
+  | otherwise = "Voce foi aprovado"
+  where med = (trab + prova)/2
+
+-- É possível definir nomes destro das funções porém
+-- esses só serão visíveis no escopo das próprias 
+-- funções
+media2 trab prova
+  | med < notaCorte = "Voce foi reprovado"
+  | otherwise = "Voce foi aprovado"
+  where med = (trab + prova)/2
+        notaCorte = 6     --Deixe alinhado assim para evitar erros de compilação
+
+
