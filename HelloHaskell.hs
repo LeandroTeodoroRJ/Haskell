495278cf7cb33fb60328198dfad20d086c81d1bf
Leandro Teodoro <leandroteodoro.rj@gmail.com>
2020-09-19 19:54:55 -0300

    Add files via upload

---
+++ HelloHaskell.hs
@@ -0,0 +1,19 @@
+main = putStrLn "Hello, Haskell!"
+
+
+-- Para compilar com o ghci digite no shell:
+-- $ghc -o hello HelloKaskell.hs
+
+-- Para executar segue a mesma forma que os 
+-- outros programas:
+-- $./hello
+
+
+-- Caso queira carregar pelo interpretador do stack ghci
+-- $stack ghci
+-- Prelude> :l HelloHaskell.hs 
+-- [1 of 1] Compiling Main             ( HelloHaskell.hs, interpreted )
+-- Ok, one module loaded.
+-- *Main> main
+-- Hello, Haskell!
+
